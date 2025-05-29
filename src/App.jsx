57d6e31f755a267cf0b477c5bf343c7d172d605a
Faisal Bhat <plesim18@gmail.com>
2025-05-29 15:55:21 +0000

    added editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from "./component/Navbar.jsx";
 import Home from "./pages/Home.jsx"
+import Editor from './pages/Editor.jsx'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
     <BrowserRouter>
      <Navbar/>
     <Routes>
-      <Route element={<Home/>}path='/'/>
+      <Route element={<Home/>} path='/'/>
+      <Route element={<Editor/>} path='/editor' />
     </Routes>
     </BrowserRouter>
     </div>
