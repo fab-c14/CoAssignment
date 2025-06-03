8070457ada832c1eeb72a8ad518aba058fb42a0b
Faisal Bhat <plesim18@gmail.com>
2025-06-03 08:58:42 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server:{
-    allowedHosts:true,
+  plugins: [react()],
+  server: {
+    allowedHosts: true,
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-   base: "/CoAssignment/",
-})
+  base: "/CoAssignment/",
+});
